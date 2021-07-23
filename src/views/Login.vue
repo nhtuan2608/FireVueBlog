4b42d42c021d40f51aeb1330666b5b3df84242d6
nhtuan2608 <nh.tuan2608@gmail.com>
2021-07-23 17:11:54 +0700

    Commit Views

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="form-wrap">
+      <form class="login">
+          <p class="login-register">
+              Don't have an account?
+              <router-link class="link" :to="{name: 'Register'}"></router-link>
+          </p>
+          <h2>Login to FireBlogs</h2>
+          <div class="inputs">
+              <div class="input">
+                  <input type="text" placeholder="" v-model="email" />
+              </div>
+          </div>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
